c44869e4aaca1466cdfae50ae64cc1d1eb50d064
danny <danny@DannyX.myguest.virtualbox.org>
2023-11-08 01:07:40 +0300

    [Feature]: Add error handler to the view

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,17 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_endpoint():
+    '''Raises a 401 error
+    '''
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden_endpont():
+    '''Raises a 403 error
+    '''
+    abort(403)
